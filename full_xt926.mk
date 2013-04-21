5e0b6bf2cd20e2da0b69707a6c569eb78e3c922b
CodeTh3ory <codeth3ory@gmail.com>
2013-04-21 00:52:08 -0400

    codex

--- full_xt926.mk
+++ full_xt926.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, device/motorola/xt926/device_xt926.mk)
+$(call inherit-product, device/motorola/xt926/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := xt926
-PRODUCT_NAME := full_xt926
+PRODUCT_NAME := codex_xt926
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := XT926
 PRODUCT_MANUFACTURER := motorola
