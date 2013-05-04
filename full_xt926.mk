b2140e0d44e9da4b0b88cb0dc1b5a971fdd13b3f
tmc363 <tmc363@droid-hive.com>
2013-05-04 08:10:16 -0500

    Update full_xt926.mk

--- full_xt926.mk
+++ full_xt926.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/motorola/xt926/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := xt926
-PRODUCT_NAME := codex_xt926
+PRODUCT_NAME := carbon_xt926
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := XT926
 PRODUCT_MANUFACTURER := motorola
